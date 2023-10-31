d92dec28fa7e682b923cc8979f8a6219db60ef0b
memicelis <elismemic5@gmail.com>
2023-10-31 16:18:23 +0100

    Fix Rubocop linters

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,48 +1,47 @@
 MORSE_CODE = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z'
-  }
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
-  def decode_char(char)
-    MORSE_CODE[char]
-  end
+def decode_char(char)
+  MORSE_CODE[char]
+end
 
-  puts decode_char(".-")
+puts decode_char('.-')
 
+def decode_word(word)
+  word.split.map { |char| decode_char(char) }.join
+end
 
-  def decode_word(word)
-    word.split(" ").map{ |char| decode_char(char)}.join("")
-  end
+puts decode_word('-- -.--   -. .- -- .')
 
-  puts decode_word("-- -.--   -. .- -- .")
+def decode(message)
+  words = message.split('  ')
+  words.map { |word| decode_word(word) }.join(' ')
+end
 
-  def decode(message)
-    words = message.split("  ")
-    words.map {|word| decode_word(word)}.join(" ")
-  end
-
-  puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
